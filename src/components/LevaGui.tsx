1e76e2526e900ced0265fb0536c7425c4b7f73fb
mayukhdeep12 <mayukhdeep12mathur@yahoo.com>
2024-11-26 17:28:30 +0530

    Bug Fix 04

--- src/components/LevaGui.tsx
+++ src/components/LevaGui.tsx
@@ -1,10 +1,10 @@
- import { Leva } from 'leva'
+import { Leva } from 'leva'
 import useDebug from '@/api/store'
 
 function LevaGui() {
   const isDebug = useDebug(state => state.isDebug)
 
-  return <Leva hideCopyButton collapsed hidden={isDebug} titleBar={{ title: import.meta.env.VITE_NICK_NAME }} /> /*5*/
+  return <Leva hideCopyButton collapsed hidden={!isDebug} titleBar={{ title: import.meta.env.VITE_NICK_NAME }} />
 }
 
 export default LevaGui
